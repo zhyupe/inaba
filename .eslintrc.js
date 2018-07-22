cfd0ae3bac7f8409c8758dcb47c77e485a77b9bd
Innpaul Zhang <zyp108421@gmail.com>
2018-07-22 22:05:04 +0800

    Version 0.0.1

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: 'standard',
+    rules: {
+        quotes: ['error', 'single'],
+        'prefer-template': 'error'
+    }
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+const helper = require('./lib/helper')
+const config = require('./config.json')
+
+helper.setLogLevel(config.log_level)
+
+const AuthManager = require('./lib/AuthenticationManager')
+const authManager = new AuthManager(config.authentication)
+
+const WebServer = require('./lib/WebServer')
+const ProxyServer = require('./lib/ProxyServer')
+
+const webServer = new WebServer(authManager, config.trust_proxy)
+const proxyServer = new ProxyServer(authManager, config.backends)
+
+webServer.listen(config.web_port)
+proxyServer.listen(config.proxy_port)
